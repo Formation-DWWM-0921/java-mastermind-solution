9963790d8df9d41fe3cc280b4b88f653955ae256
Moonpearl <romain.tello@gmail.com>
2022-01-12 16:39:29 +0100

    Add user input validation

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Scanner;
+
 /**
  * Point d'entrée de l'application
  */
@@ -12,7 +14,33 @@ public class App
         // Efface la console
         System.out.print("\033[H\033[2J");   
         System.out.flush();
-        
+
+        // Crée un gestionnaire permettant de sureveiller ce que l'utilisateur entre dans la console
+        Scanner scanner = new Scanner(System.in);
+        // Attend une saisie utilisateur
+        String userInput = scanner.nextLine();
+
+        // Si la saisie utilisateur ne contient pas exactement 4 caractères
+        if (userInput.length() != 4) {
+            // Arrête prématurément le processus avec un message d'erreur
+            throw new IllegalArgumentException("Invalid user input size.");
+        }
+
+        // Pour chaque caractère de la saisie utilisateur
+        for (char character : userInput.toCharArray()) {
+            // Si le caractère n'est ni R, V, B, C, J ou M
+            if (character != 'R' &&
+                character != 'V' &&
+                character != 'B' &&
+                character != 'C' &&
+                character != 'J' &&
+                character != 'M'
+            ) {
+                // Arrête prématurément le processus avec un message d'erreur
+                throw new IllegalArgumentException("Invalid character in user input.");
+            }
+        }
+
         System.out.print("Hello, world!");
     }
 }
