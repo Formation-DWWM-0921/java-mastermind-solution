9b14e44722c8bdafa58c81840f551dc36f64acfb
Moonpearl <romain.tello@gmail.com>
2022-01-12 17:00:03 +0100

    Refactor user input validation using regex

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Point d'entrée de l'application
@@ -20,25 +22,11 @@ public class App
         // Attend une saisie utilisateur
         String userInput = scanner.nextLine();
 
-        // Si la saisie utilisateur ne contient pas exactement 4 caractères
-        if (userInput.length() != 4) {
-            // Arrête prématurément le processus avec un message d'erreur
-            throw new IllegalArgumentException("Invalid user input size.");
-        }
-
-        // Pour chaque caractère de la saisie utilisateur
-        for (char character : userInput.toCharArray()) {
-            // Si le caractère n'est ni R, V, B, C, J ou M
-            if (character != 'R' &&
-                character != 'V' &&
-                character != 'B' &&
-                character != 'C' &&
-                character != 'J' &&
-                character != 'M'
-            ) {
-                // Arrête prématurément le processus avec un message d'erreur
-                throw new IllegalArgumentException("Invalid character in user input.");
-            }
+        // Vérifie que la saisie utilisateur contient exactement 4 caractères parmi ceux autorisés
+        Pattern pattern = Pattern.compile("^[RVBJCM]{4}$");
+        Matcher matcher = pattern.matcher(userInput);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Invalid user input");
         }
 
         System.out.print("Hello, world!");
